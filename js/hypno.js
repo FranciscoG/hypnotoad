5e3b2bebfbd675a026e6251bfdf35e04dcb304ca
franciscog <frankjg@gmail.com>
2017-06-21 00:44:22 -0400

    sound in js is close

---
+++ js/hypno.js
@@ -0,0 +1,37 @@
+
+/*
+  sources
+  
+  A good explanation and recreation of the sound (with some history of it too)
+  http://www.scottsmitelli.com/articles/everybody-imitates-hypnotoad
+  
+  How to make Brownian noise with Web Audio API
+  but I didn't use it
+  https://noisehack.com/generate-noise-web-audio-api/
+  
+  p5 sound!
+  https://github.com/processing/p5.js-sound
+ */
+
+/* global p5 */
+
+
+var noise = new p5.Noise('brown');
+
+var delay = new p5.Delay();
+delay.process(noise, 0.025, 0.8, 8380);
+
+var isplaying = false;
+
+var btn = document.getElementById('start');
+btn.addEventListener('click', function(e){
+  if (!isplaying) {
+    e.target.textContent = "Stop";
+    noise.start();
+    isplaying = true;
+  } else {
+    e.target.textContent = "Make some noise";
+    noise.stop();
+    isplaying = false;
+  }
+});
